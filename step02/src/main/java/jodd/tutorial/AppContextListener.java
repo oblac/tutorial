fc8ccf2345be9585403f724091055bcd467c9334
Igor Spasić <igor@jodd.org>
2017-12-17 01:03:53 +0100

    step02

---
+++ step02/src/main/java/jodd/tutorial/AppContextListener.java
@@ -0,0 +1,10 @@
+package jodd.tutorial;
+
+import jodd.joy.JoyContextListener;
+
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class AppContextListener extends JoyContextListener {
+
+}
